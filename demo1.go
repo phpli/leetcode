c6746b933552cbcdfcc32d4eaa447c6f5a71e5af
phpist <lichangdong100@gmail.com>
2021-03-20 21:30:54 +0800

    leetcode 001

---
+++ demo1.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+func main() {
+	var nums = []int{2, 7, 11, 15}
+	var target = 9
+	fmt.Println(twoSum(nums, target))
+}
+
+func twoSum(nums []int, target int) []int {
+	m := make(map[int]int)
+	for i := 0; i < len(nums); i++ {
+		//fmt.Println(m[i])
+		another := target - nums[i]
+		//fmt.Println(another)
+		if _, ok := m[another]; ok {
+			return []int{m[another], i}
+		}
+		m[nums[i]] = i
+		//fmt.Println(m)
+	}
+	return nil
+}
