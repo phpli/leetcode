9f4fbd118d07f07426d13382d4b7e6bdd231e565
phpli <lichangdong100@gmail.com>
2021-04-01 15:55:13 +0800

    leetcode0011

---
+++ demo2.go
@@ -0,0 +1,28 @@
+package main
+
+import "fmt"
+
+func main()  {
+	height :=  []int{1,8,6,2,5,4,8,3,7}
+	fmt.Println(maxArea(height))
+}
+
+func maxArea(height []int) int  {
+	start, end, max := 0, len(height)-1, 0
+	for start < end {
+		high := 0
+		width := end - start
+		if height[start] < height[end] {
+			high = height[start]
+			start++
+		} else {
+			high = height[end]
+			end--
+		}
+		temp := width*high
+		if temp > max {
+			max = temp
+		}
+	}
+	return max
+}
