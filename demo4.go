fbaf68577be3f0022f8fe6feea77da3a02145731
changdong <lichangdong100@gmail.com>
2024-04-20 22:01:32 +0800

    test

--- demo4.go
+++ demo4.go
@@ -5,10 +5,10 @@ import "fmt"
 /**
 \ 反转单链表
 那么这道题其实就是把指针指向前一个节点
- */
+*/
 
 type ListNode struct {
-	data interface{}
+	data int
 	Next *ListNode
 }
 
@@ -17,12 +17,16 @@ func reverseList(head *ListNode) *ListNode {
 	cur := head
 	var pre *ListNode = nil
 	for cur != nil {
-		pre, cur, cur.Next = cur, cur.Next,pre //
+		//pre, cur, cur.Next = cur, cur.Next, pre //
+		nextTemp := cur.Next
+		cur.Next = pre
+		pre = cur
+		cur = nextTemp
 	}
 	return pre
 }
 
-func CreateNode(node *ListNode, max int)  {
+func CreateNode(node *ListNode, max int) {
 	cur := node
 	for i := 1; i < max; i++ {
 		cur.Next = &ListNode{}
@@ -31,17 +35,17 @@ func CreateNode(node *ListNode, max int)  {
 	}
 }
 
-func PrintNode(info string, node *ListNode)  {
+func PrintNode(info string, node *ListNode) {
 	fmt.Print(info)
 	for cur := node; cur != nil; cur = cur.Next {
-		fmt.Print(cur.data,"")
+		fmt.Print(cur.data, "")
 	}
 	fmt.Println()
 }
 
 func main() {
-	var head = new(ListNode)
-	CreateNode(head, 10)
+	head := &ListNode{data: 1, Next: &ListNode{data: 2, Next: &ListNode{data: 3, Next: &ListNode{data: 4, Next: nil}}}}
+	//CreateNode(head, 10)
 	PrintNode("前：", head)
 	yyy := reverseList(head)
 	PrintNode("后：", yyy)
